f63f10dcee6a7dfc13df463bc6012581a615dfe5
zhaozhiming <zhaogoodwell@gmail.com>
2014-05-01 13:14:51 +0800

    计数不漂

--- src/projects/bigbang/isr.c
+++ src/projects/bigbang/isr.c
@@ -29,7 +29,6 @@ u8  image_handle_flag = 0;      //图像处理标志
 
 void PORTE_IRQHandler()     //场中断来临
 {
-    Light2_turn();
     if(PORTE_ISFR & (1 << 11))
     {
         PORTE_ISFR |= (1 << 11);     
@@ -37,22 +36,19 @@ void PORTE_IRQHandler()     //场中断来临
         vert_flag = 1;  //场标志置位
         row_count = 0;  //行计数清零
         DMA_count_reset(DMA_CH0);
-        
-//        Light1_turn();
     }
 }
 
-
+unsigned int count2_temp;
 void PORTD_IRQHandler()     //行中断来临
 {
     u8 i; //计数。
-    static unsigned int count2;
-    Light1_turn();
+   static unsigned int count2;
+    
     if(PORTD_ISFR & (1<<14))
     {
         PORTD_ISFR |= (1 << 14);
-        if(vert_flag ==0)   return;     //不足一场时返回         
-        
+        if(vert_flag ==0)   return;     //不足一场时返回        
         row_count = DMA_count_get(DMA_CH0);
         
         /*******************控制部分*********************/
@@ -60,24 +56,27 @@ void PORTD_IRQHandler()     //行中断来临
         {
             /*********滤波及控制算法**********/
             //最小也大约有2.5ms的时间做控制
-              
+            
               car->angle_m = acc_data_get();
               car->gyro_m = gyro_data_get();
               blance_kalman_filter(car);
+
+//              OutData[0] = car->angle;
+//              OutData[1] = car->angle_m;
+//              OutData[2] = car->gyro;
+//              OutData[3] = car->gyro_m;
+//              send_toscope();
               
-////              OutData[0] = car->angle_m;
-////              OutData[1] = car->gyro_m;
-////              OutData[2] = car->angle;
-////              OutData[3] = 0;
-////              send_toscope();
- //              printf("%f\t%f\t%f\t%f\n",car->angle_m,car->gyro_m,car->angle,car->left_duty);
+//              printf("%f\t%f\t%f\t%f\n",car->angle,car->angle_m,car->gyro,car->gyro_m);
          
            count2++;
-//           printf("%d\n",count2);
+           count2_temp = count2;
+         //  printf("%d\n",count2);
           if(count2==20)
           {
                car->speed_right_m  =  right_speed();
                car->speed_left_m   =  (car->speed_right_m >= 0)? left_speed(): -left_speed();
+          //   printf("%d\t%d\n",car->speed_left_m,car->speed_right_m);
                speed_control(car);
                count2 = 0;
           }
@@ -86,26 +85,28 @@ void PORTD_IRQHandler()     //行中断来临
          car->left_duty     = car->blance_duty - car->speed_duty + car->direction_left_duty;
          car->right_duty    = car->blance_duty - car->speed_duty - car->direction_right_duty;
          motor_set(car);  
-        printf("%f\t%f\n", car->left_duty ,car->right_duty);
+         
+    //     printf("%d\n",row_count);
+    //     gpio_turn(PORTC,14);
+
         }
+        
         /********************图像处理部分*********************/
         if((row_count > 161)&&(image_handle_flag == 0))         //第三次控制算法已完成且图像未处理
         {
             /**********图像处理************/
-            
-                for(i=0;i<40;i++)
-                {
-                  edge_l[i] = image_left_offset(image , i);
-                  edge_r[i] = image_right_offset(image , i);
-                }
-                image_err(car, 0 ,39);
-                direction_control(car);
-               // printf("%f\t%f\t%f\n",car->direction,car->direction_left_duty,car->direction_err_new);
-                image_handle_flag = 1;      //图像处理标志置1,图像处理及方向计算完成
+        
+            for(i=0;i<40;i++)
+            {
+              edge_l[i] = image_left_offset(image , i);
+              edge_r[i] = image_right_offset(image , i);
+            }
+            image_err(car, 0 ,39);
+            direction_control(car);
+           // printf("%f\t%f\t%f\n",car->direction,car->direction_left_duty,car->direction_err_new);
+            image_handle_flag = 1;      //图像处理标志置1,图像处理及方向计算完成
         }
          
-            
-        
         /******************图像采集部分*******************/
         if(row_count < ROW_START)   return;     //未到需要采集的行
         else if(row_count > ROW_END)
@@ -115,8 +116,8 @@ void PORTD_IRQHandler()     //行中断来临
         }
         else
         {
-//          camera_wait();      //越过消隐区
-            DMA_PORTx2BUFF_Init (DMA_CH4, (void *)&PTB_BYTE0_IN, image[row_count-ROW_START], PTD5, DMA_BYTE1, COL, DMA_rising);
+    //          camera_wait();      //越过消隐区
+            DMA_PORTx2BUFF_Init (DMA_CH4, (void *)&PTE_BYTE0_IN, image[row_count-ROW_START], PTE27, DMA_BYTE1, COL, DMA_rising);
             //----使能DMA，初始化的时候禁止DMA
             DMA_EN(DMA_CH4); 
         }
@@ -133,11 +134,22 @@ void DMA_CH4_Handler(void)
     //DMA通道4
     DMA_IRQ_CLEAN(DMA_CH4); //清除通道传输中断标志位    (这样才能再次进入中断)
     
-    DMA_EN(DMA_CH4);          //使能通道CHn 硬件请求      (这样才能继续触发DMA传输)                             //使能通道CHn 硬件请求      (这样才能继续触发DMA传输)
+    DMA_EN(DMA_CH4);                                    //使能通道CHn 硬件请求      (这样才能继续触发DMA传输)
 }
 
 
 /************************************************************************************************************************************/
+
+
+u32 a,b,c,d;
+
+void PIT_CH0_Handler()
+{
+    PIT_Flag_Clear(PIT0);
+//    car->speed_left_m=pulse_cnt_left();
+//    car->speed_right_m= pulse_cnt_right();
+//    speed_control(car);
+}
 
 void PIT_CH1_Handler(void)
 {
